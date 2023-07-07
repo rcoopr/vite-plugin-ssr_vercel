7e108e39d3e10952c1f59b295a683ec21e420a73
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2023-07-07 15:39:50 +0100

    use urlParsed

--- renderer/_default.page.server.js
+++ renderer/_default.page.server.js
@@ -1,22 +1,22 @@
-import { renderToString } from '@vue/server-renderer'
-import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
-import { createApp } from './app'
-import logoUrl from './logo.svg'
+import { renderToString } from '@vue/server-renderer';
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr';
+import { createApp } from './app';
+import logoUrl from './logo.svg';
 
-export { render }
-export { passToClient }
+export { render };
+export { passToClient };
 
 // See https://vite-plugin-ssr.com/data-fetching
-const passToClient = ['pageProps']
+const passToClient = ['pageProps'];
 
 async function render(pageContext) {
-  const app = createApp(pageContext)
-  const appHtml = await renderToString(app)
+  const app = createApp(pageContext);
+  const appHtml = await renderToString(app);
 
   // See https://vite-plugin-ssr.com/head
-  const { documentProps } = pageContext
-  const title = documentProps?.title || 'Vite SSR app'
-  const desc = documentProps?.description || 'App using Vite + vite-plugin-ssr'
+  const { documentProps } = pageContext;
+  const title = documentProps?.title || 'Vite SSR app';
+  const desc = documentProps?.description || 'App using Vite + vite-plugin-ssr';
 
   return escapeInject`<!DOCTYPE html>
     <html lang="en">
@@ -25,10 +25,11 @@ async function render(pageContext) {
         <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
+        <meta name="og:image" content="${pageContext.urlParsed.searchOriginal}" />
         <title>${title}</title>
       </head>
       <body>
         <div id="app">${dangerouslySkipEscape(appHtml)}</div>
       </body>
-    </html>`
+    </html>`;
 }
